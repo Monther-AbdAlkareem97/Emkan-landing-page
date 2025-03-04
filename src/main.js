76f0fd5bd1fb75fb5081efedc5223bb11a15380f
Monther-AbdAlkareem97 <eng20154122@omu.edu.ly.com>
2025-03-04 22:28:16 +0200

    vv

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(router).mount('#app')
+library.add(faHome, faUser);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+createApp(App).use(router).mount("#app");
